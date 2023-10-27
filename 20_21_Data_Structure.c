f762819c6cc0bf537d77d8705858419e9522de01
Mojahidul21 <148482250+Mojahidul21@users.noreply.github.com>
2023-10-27 18:31:52 +0600

    Four features are compiled in one
    Four in one compiled done. Next step is to go through functions. And finally switch case.

--- 20_21_Data_Structure.c
+++ 20_21_Data_Structure.c
@@ -1,4 +1,44 @@
-/*//Bubble Sort
+/*1) Linear Search
+
+#include<stdio.h>
+
+int main(){
+    printf("How many numbers do you want to enter?: ");
+    int n;
+    scanf("%d",&n);
+
+    int a[n],i;
+    for (i=0;i<n;i++){
+        printf("\nPlease enter the %d st/nd/rd/th element: ",i+1);
+        scanf("%d",&a[i]);
+    }
+
+    printf("\nPlease enter the number you want to search: ");
+    int x;
+    scanf("%d",&x);
+
+
+    int counter=0;
+    for(i=0;i<n;i++){
+        if(a[i]==x){
+            printf("\n%d is found at the position %d.\n",x,i+1);
+        }
+        else{
+            counter++;
+        }
+    }
+
+    if(counter==n){
+        printf("\n%d is not found at the position nowhere!\n",x);
+    }
+
+    return 0;
+}*/
+
+
+
+/*2) Bubble Sort
+
 # include<stdio.h>
 int main(){
     printf("Please enter array size: ");
@@ -38,7 +78,82 @@ int main(){
 
 
 
-//Insertion sort
+/*3) Binary Search
+
+#include<stdio.h>
+int main(){
+    printf("How many number(s) do you want to enter?: ");
+    int n;
+    scanf("%d",&n);
+    for(;n<1;){
+        printf("\nPlease enter at least one!: ");
+        scanf("%d",&n);
+    }
+
+    printf("\nCaution! You must enter the numbers in ascending order!\n\n");
+    int arr[n],i;
+
+    for(i=0;i<n;i++){
+        if(i==0){
+            printf("Enter the 1st element: ");
+        }
+        else if(i==1){
+            printf("Enter the 2nd element: ");
+        }
+        else if(i==2){
+            printf("Enter the 3rd element: ");
+        }
+        else{
+            printf("Enter the %dth element: ",i+1);
+        }
+        scanf("%d",&arr[i]);
+
+    }
+
+    printf("\nNow enter the number you want to search: ");
+    int x;
+    scanf("%d",&x);
+
+        int n1=2*n,mid,s=0,e=n-1;
+
+        for(i=1;i<=n;i++){
+            mid=arr[(s+e)/2];
+
+            if((x<arr[0])||(x>arr[n-1])){
+                printf("\nYou are searching for %d and it is located at the position nowhere!\n",x);
+                break;
+            }
+
+            if(s==e&&x!=mid){
+                printf("\nYou are searching for %d and it is located at the position nowhere!\n",x);
+                break;
+            }
+
+            if(s>e){
+                printf("\nYou are searching for %d and it is located at the position nowhere!\n",x);
+                break;
+            }
+
+            if(x==mid){
+                printf("\nYou are searching for %d and it is located at the position %d.\n",x,(s+e)/2+1);
+                break;
+            }
+            else if(x<mid){
+                e=(s+e)/2-1;
+            }
+            else if(x>mid){
+                s=(s+e)/2+1;
+            }
+            else{
+            }
+        }
+
+    return 0;
+}*/
+
+
+
+/*4) Insertion sort
 
 #include<stdio.h>
 
@@ -71,4 +186,4 @@ int main(){
     }
 
     return 0;
-}
+}*/
